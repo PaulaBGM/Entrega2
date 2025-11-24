3492490a496eeaac67c139d35b4bccf36a74bdb1
NxCzTRx <ismabaugra@gmail.com>
2025-11-24 19:01:06 +0100

    bugfix: tools adjusted

--- Assets/Scripts/Items/MagnifyingGlass.cs
+++ Assets/Scripts/Items/MagnifyingGlass.cs
@@ -4,17 +4,5 @@ using Items;
 
 public class MagnifyingGlass : ItemBase
 {
-    private Vector2 dragOffset;
 
-    private void OnMouseDown()
-    {
-        Vector2 mouseWorld = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        dragOffset = mouseWorld - new Vector2(transform.position.x, transform.position.y);
-    }
-
-    private void OnMouseDrag()
-    {
-        Vector2 mouseWorld = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        transform.position = mouseWorld - dragOffset;
-    }
 }
